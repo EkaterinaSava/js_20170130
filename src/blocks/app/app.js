b1cfb7c34945dadb161cc5a535fa3b4a7de6ad8b
Ekaterina Sava <ekaterinasava@gmail.com>
2017-04-03 21:24:42 +0300

    updapp.js: add data & add notes-stuff

--- src/blocks/app/app.js
+++ src/blocks/app/app.js
@@ -1,7 +1,33 @@
-import Login from '../login/login.js';
-import Signin from '../signin/signin.js';
+import Login from '../login/login';
+import Signup from '../signup/signup';
+import Note from '../note/note';
+import NotesList from '../notesList/notesList';
 
-document.addEveniListener('DOMContentLoaded', () => {
-  const login = new Login(document.querySelector('.js__login-view'));
-  const signup = new Signup(document.querySelector('.js__signup-view'));
+const data = [
+  {
+    header: 'Заметка 1',
+    text: 'Текст заметки 1',
+    color: 'red',
+  },
+  {
+    header: 'Заметка 2',
+    text: 'Текст заметки 2',
+    color: 'green',
+  }
+];
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const login = new Login(document.querySelector('.js-login-view'));
+  const signup = new Signup(document.querySelector('.js-signup-view'));
+  const notesList = new NotesList(document.querySelector('.js-notes-list'), data);
+
+  data.forEach((element) => {
+    const note = new Note({
+    	node: document.querySelector('.js-note-view'),
+    	header: element.header,
+    	text: element.text,
+    	color: element.color,
+    });
+  });
 });

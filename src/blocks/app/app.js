dd9cd4338f848d1a8fa68450dbc7d729007e1a76
Ekaterina Sava <ekaterinasava@gmail.com>
2017-04-05 14:37:42 +0300

    upd all /blocks: js + xml, add /logout block

--- src/blocks/app/app.js
+++ src/blocks/app/app.js
@@ -1,7 +1,9 @@
 import Login from '../login/login';
+import Logout from '../logout/logout';
 import Signup from '../signup/signup';
 import Note from '../note/note';
 import NotesList from '../notesList/notesList';
+import Cookies from 'js-cookie';
 
 const data = [
   {
@@ -16,18 +18,35 @@ const data = [
   }
 ];
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const login = new Login(document.querySelector('.js-login-view'));
-  const signup = new Signup(document.querySelector('.js-signup-view'));
+function initNotesList() {
   const notesList = new NotesList(document.querySelector('.js-notes-list'), data);
 
   data.forEach((element) => {
     const note = new Note({
-    	node: document.querySelector('.js-note-view'),
-    	header: element.header,
-    	text: element.text,
-    	color: element.color,
+      node: document.querySelector('.js-note-view'),
+      header: element.header,
+      text: element.text,
+      color: element.color
     });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const user = Cookies.get('user');
+
+  if (!user) {
+    const login = new Login(document.querySelector('.js-login-view'));
+    const signup = new Signup(document.querySelector('.js-signup-view'));
+  } else {
+    const logout = new Logout(document.querySelector('.js-logout-view'));
+    initNotesList();
+  }
+
+  document.body.addEventListener('user.login', () => {
+    document.querySelector('.js-login-view').innerHTML = '';
+    document.querySelector('.js-signup-view').innerHTML = '';
+
+    const logout = new Logout(document.querySelector('.js-logout-view'));
+    initNotesList();
   });
 });
--- src/blocks/signup/signup.js
+++ src/blocks/signup/signup.js
@@ -1,10 +1,11 @@
 import template from './signup.xml.js';
 
 export default class Signup {
-
   constructor(node) {
     this.node = node;
     this.render();
+    this.cache();
+    this.events();
   }
 
   render() {
@@ -12,9 +13,57 @@ export default class Signup {
       texts: {
         login: 'Login',
         password: 'Password',
-        email: 'Email',
-      },
+        email: 'Email'
+      }
     });
   }
 
+  cache() {
+    this.formName = 'formSingup';
+  }
+
+  events() {
+    document.querySelector('.js-signup-view').addEventListener('submit', this.submit.bind(this));
+  }
+
+  submit(ev) {
+    const form = ev.target;
+    const url = form.getAttribute('action');
+
+    if (form.getAttribute('name') !== this.formName) {
+      return;
+    }
+
+    ev.preventDefault();
+
+    const xhr = new XMLHttpRequest();
+    const data = JSON.stringify({
+      email: form.email.value,
+      login: form.login.value,
+      password: form.password.value
+    });
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.status !== 200) {
+        alert(xhr.status + ': ' + xhr.statusText);
+      } else {
+        const response = JSON.parse(xhr.responseText);
+        alert(response.message);
+
+        if (response.status === 'success') {
+          const event = new CustomEvent('user.login');
+          document.body.dispatchEvent(event);
+        }
+      }
+    });
+
+    xhr.send(data);
+  }
 }
